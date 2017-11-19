b64b9ecc712f9bbbaa62281ebc5b567c32e3e6d2
KetkiMahajan <mahajanketki24@gmail.com>
2017-11-19 16:34:30 -0600

    Ketki2
    Adding procedure call

--- src/php/homepage.php
+++ src/php/homepage.php
@@ -3,7 +3,6 @@ include 'connection.php';
 session_start();
 $session_username="";
 $session_username= $_SESSION['username'];
-echo "$session_username";
 
     $sql="SELECT name FROM cr_student WHERE user_id= '$session_username'" ;
     $result = mysqli_query($conn, $sql);
@@ -11,21 +10,42 @@ echo "$session_username";
       while($row = mysqli_fetch_assoc($result)){
         echo "<h1>Welcome,". $row["name"]. "</h1>";
       }
+    }
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+      $major=$_POST["Major"];
+      $degree=$_POST["Degree"];
+      $sem=$_POST["Semester"];
 
     }
+    echo "$major";
+    echo "$degree";
+    echo "$sem";
+    $sql2="";
+    $result2 = mysqli_query($conn, $sql2);
 
+		if (mysqli_num_rows($result2) > 0) {
+					while($row = mysqli_fetch_assoc($result)) {
+						echo "<tr>";
+						echo "<td>" . $row['name'] . "</td>";
+						echo "<td>" . $row['year'] . "</td>";
+						echo "<td>" . $row['ranking'] . "</td>";
+						echo "<td>" . $row['gender'] . "</td>";
+						echo "</tr>";
+					}
+		}
 ?>
 <html lang="en">
 <head>
     <title>Online Course Registration</title>
     <link type="text/css" rel="stylesheet" href="../css/common.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 </head>
 <body>
     <section>
   		<h2>Online Course Registration login</h2>
-    <form action="#" method="post">
-
+<form action="#" method="POST">
       <label>Degree:</label>
       <select name="Degree">
         <option value="Graduate">Graduate</option>
@@ -36,11 +56,12 @@ echo "$session_username";
 
       <label>Major:</label>
       <select name="Major">
-        <option value="Computer Science">Computer Science</option>
-        <option value="itm">ITM</option>
-        <option value="Electrical Engineering">Electrical Engineering</option>
-        <option value="Mechanical Engineering">Mechanical Engineering</option>
-        <option value="ALl">All</option>
+
+        <option value="Computer Science"<?= $_REQUEST["Major"]=="Computer Science"?" selected='selected'":"" ?>>Computer Science</option>
+        <option value="Mechanical Engineering"<?= $_REQUEST["Major"]=="Mechanical Engineering"?" selected='selected'":"" ?>>Mechanical Engineering</option>
+        <option value="itm"<?= $_REQUEST["Major"]=="itm"?" selected='selected'":"" ?>>ITM</option>
+        <option value="Electrical Engineering"<?= $_REQUEST["Major"]=="Electrical Engineering"?" selected='selected'":"" ?>>Electrical Engineering</option>
+        <option value="All"<?= $_REQUEST["Major"]=="All"?" selected='selected'":"" ?>>All</option>
       </select>
 
       <label>Semester:</label>
@@ -49,9 +70,9 @@ echo "$session_username";
           <option value="Spring18">Spring18</option>
         <option value="ALl">All</option>
       </select>
+        <button id="search">Search</button>
+
+      </form>
     </section>
 </body>
 </html>
-<?php
-
- ?>
